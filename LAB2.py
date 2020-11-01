b139cc5d22741569f0a10582616691bc750333e3
taufiqalif <aliftaufiq4@gmail.com>
2020-11-01 19:27:37 +0700

    file python biodata

---
+++ LAB2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+a = eval(input("masukan nilai a: "))
+b = eval(input("masukan nilai b: "))
+hasil = a+b
+hasil1 = a/b
+hasil2 = a*b
+print("variable a= ", a)
+print("variable b= ", b)
+print("hasil penggabungan %d & %d = %d" % (a, b, hasil))
+print("hasil penggabungan %d / %d = %d" % (a, b, hasil1))
+print("hasil penggabungan %d * %d = %d" % (a, b, hasil2))
+print("hasil penggabungan %d + %d = %d" % (a, b, hasil))
